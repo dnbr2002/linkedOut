7fe0a271986936c359337ae3d72e2958aae0b553
dnbr2002 <dnbr2002@yahoo.com>
2016-09-22 15:30:52 -0400

    updated db

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(bodyParser.json());
 
 app.use('/', express.static('public'));
 
+
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
--- db.js
+++ db.js
@@ -3,7 +3,47 @@ var db = new sqlite3.Database('./lidb.sqlite');
 
 
 
+exports.AuthenticateUser = AuthenticateUser;
+function AuthenticateUser(Obj){
+    return new Promise(
+            (resolve, reject) => {
+            //logic
+            var table = "User";
+    var where = " WHERE email = '" + obj + "'";
+    var sql = "SELECT PK_User, Name FROM " + table + where + ";";
+    //console.log(sql);
+    db.all(sql, function (err, rows) {
+        if (err) {
+            //console.log(err);
+            reject("failed!");
+            return;
+        }
+        //console.log("typeof rows " + (typeof rows[0]));
+        if(typeof rows[0] === "undefined"){
+            reject("failed!");
+            return;
+        }else{
+            // console.log("PK_User " + rows[0].PK_User);
+            // console.log("Name " + rows[0].Name);
+            // console.log("obj " + obj);
 
+            if(rows[0].Name == obj){
+                // console.log("row id" + rows.PK_User);
+                // console.log("username " +rows.Name  + " pk " + rows.PK_User);
+                resolve(rows[0].PK_User);
+                return;
+            }else{
+                reject("failed!");
+                return;
+            }
+
+        }
+
+        //console.log(rows);
+        //resolve(row);
+    });
+});
+};
 
 
 
