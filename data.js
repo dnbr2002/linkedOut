da4c988639b2929ffa2e8178698ccefd56bf949b
Mutt_n_head <lewis.isner@gmail.com>
2016-09-26 18:12:12 -0400

    fu

--- data.js
+++ data.js
@@ -187,46 +187,53 @@ function doSQL(sqlStr, bindings, cb) {
     );
 }
 
-// exports.getUserFeed = getUserFeed;
-// function getUserFeed(userid, cb) {
-//     // Returns an array of the pk_users the user id is Following
-//     var posts = {};
-//     var comments = {};
-//
-//     var sqlStr =
-//         "select * from "
-//         + "post p "
-//         + "LEFT OUTER JOIN "
-//         + "photo ph on p.photoid = ph.pk_photo "
-//         + "INNER JOIN following f on f.followeeid = p.userid "
-//         + "INNER JOIN user u on u.pk_user = p.userid "
-//         + "WHERE f.followerid = "
-//         + userid;
-//
-//
-//     var p = new Promise(function(resolve, reject) {
-//         db.serialize(function() {
-//             console.log('Running ' + sqlStr);
-//             db.all(sqlStr, function(err, rows)) {
-//                 var posts[];
-//                 var comments[];
-//
-//                 for (row in rows) {
-//                     if (row.referencepost == null) {
-//                         posts.push(row);
-//                     } else {
-//                         comments.push(row);
-//                     }
-//                 }
-//
-//                 for (post in posts) {
-//                     for (comment in comments) {
-//                         if (comment.referencepost === post.pk_post)
-//                     }
-//                 }
-//             }
-//         });
-//     });
+exports.getUserFeed = getUserFeed;
+function getUserFeed(userid, cb) {
+    // Returns an array of the pk_users the user id is Following
+    var posts = {};
+    var comments = {};
+
+    var sqlStr =
+        "select * from "
+        + "post p "
+        + "LEFT OUTER JOIN "
+        + "photo ph on p.photoid = ph.pk_photo "
+        + "INNER JOIN following f on f.followeeid = p.userid "
+        + "INNER JOIN user u on u.pk_user = p.userid "
+        + "WHERE f.followerid = "
+        + userid;
+
+
+    var p = new Promise(function(resolve, reject) {
+        db.serialize(function() {
+            var sql = "SELECT * FROM POST WHERE USERID = " + userid;
+
+            db.all(sqlStr, function(err, rows)) {
+                var posts[];
+                var comments[];
+
+                for (row in rows) {
+                    if (row.referencepost == null) {
+                        posts.push(row);
+                    } else {
+                        comments.push(row);
+                    }
+                }
+
+                for (comment of comments) {
+                    var refPost = comment.referencepost;
+
+                    // If the posts object doesn't already exist, add it
+                    if (posts[refPost] == undefined) {
+
+                        var postComments = posts[comment.referencepost]['comments'];
+
+                    }
+                }
+            }
+        });
+    });
+}
 
 exports.getEducation = getEducation;
 function getEducation(userid, cb) {
