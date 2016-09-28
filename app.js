6aaadad0217e6858cc3f8cf51fe3ea801b976c75
Mutt_n_head <lewis.isner@gmail.com>
2016-09-28 15:08:16 -0400

    Changed pic locationi

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.post('/adduser', function (req, res) {
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './public/uploads/')
+        cb(null, './public/photos/post')
     },
     filename: function (req, file, cb) {
         req.body.generatedname = new Date().getTime() + file.originalname;
