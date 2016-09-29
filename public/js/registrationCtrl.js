a687f87a0cb31f84c601db97884de6a88317db4e
mutt-n-head <abbyisgr8>
2016-09-29 17:16:28 -0400

    oops

--- public/js/registrationCtrl.js
+++ public/js/registrationCtrl.js
@@ -1,36 +1,5 @@
-<<<<<<< HEAD
 'use strict';
 
-angular.module('tutorialWebApp').controller('loginCtrl', function($scope, $location, $rootScope, DataService, currentUser)
-{
-    // Method that calls the RESTful API.
-    $scope.register = function()
-    {
-        $scope.regError = "";
-        var params =
-        {
-            username: $scope.username,
-            fullname: $scope.fullname,
-            password: $scope.password
-        };
-        DataService.postData('/adduser', params).success(
-            function(response)
-            {
-                currentUser.email = response.username;
-                currentUser.fullname = response.fullname;
-
-                $location.path('/home');
-            }
-        ).error(
-            function(err)
-            {
-                $scope.regError = "Invalid password.";
-            }
-        );
-
-    }
-}
-=======
 angular.module('tutorialWebApp').controller('registrationCtrl', function($scope, $location, $rootScope, $http, DataService, currentUser)
 {
     console.log("registrationCtrl called");
@@ -77,4 +46,3 @@ angular.module('tutorialWebApp').controller('registrationCtrl', function($scope,
         $scope.registrationError = '';
     }
 });
->>>>>>> 842f7acdda2b2c783c62e139cb14f02abe5fa10f
